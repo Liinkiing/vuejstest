a61fdb6d0ac1b84ac907f41a41e7336126a94abd
Omar Jbara <omar.jbara2@gmail.com>
2016-11-19 22:16:20 +0100

    Added remove todo and remove all done todos

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,17 +4,18 @@
       <input type="text" class="new-todo" v-model="newTodo" @keyup.enter="addTodo" placeholder="Ajoutez une tâche">
     </header>
     <section class="main">
+      <input type="checkbox" class="toggle-all" v-model="allDone">
       <ul class="todo-list">
         <li class="todo" :class="{'completed': todo.complete}" v-for="todo in filteredTodos">
           <div class="view">
             <input type="checkbox" v-model="todo.complete" class="toggle">
             <label>{{ todo.name }}</label>
-            <button class="destroy"></button>
+            <button @click.prevent="removeTodo(todo)" class="destroy"></button>
           </div>
         </li>
       </ul>
     </section>
-    <footer class="footer">
+    <footer class="footer" v-if="todos.length > 0">
       <span class="todo-count">
         <strong> {{ remainingTodos }} </strong> tâches à faire
       </span>
@@ -23,6 +24,9 @@
         <li><a :class="{'selected': filter === 'todo'}" @click.prevent="changeFilter('todo')" href="#">A faire</a></li>
         <li><a :class="{'selected': filter === 'done'}" @click.prevent="changeFilter('done')" href="#">Faites</a></li>
       </ul>
+      <button class="clear-completed" v-if="getDoneTodos.length > 0" @click.prevent="removeDoneTodos()">
+        Sup. tâches faites
+      </button>
     </footer>
   </section>
 
@@ -45,27 +49,46 @@
     },
     computed: {
       remainingTodos () {
-        return this.todos.filter(todo => !todo.complete ).length
+        return this.todos.filter(todo => !todo.complete).length
       },
       filteredTodos () {
-        switch (this.filter){
+        switch (this.filter) {
           case 'all':
-            return this.todos.filter((todo, index, array) => array );
+            return this.todos.filter((todo, index, array) => array);
             break;
           case 'todo':
-            return this.todos.filter(todo => !todo.complete );
+            return this.todos.filter(todo => !todo.complete);
             break;
           case 'done':
-            return this.todos.filter(todo => todo.complete );
+            return this.todos.filter(todo => todo.complete);
             break;
         }
 
+      },
+      getDoneTodos() {
+        return this.todos.filter(todo => todo.complete);
+      },
+      allDone: {
+        get () {
+          return this.remainingTodos === 0
+        },
+        set (value) {
+          this.todos.forEach(todo => {
+            todo.complete = value
+          })
+        }
       }
     },
     methods: {
       addTodo() {
         this.todos.push({name: this.newTodo, complete: false});
         this.newTodo = '';
+      },
+      removeTodo(todo) {
+        this.todos = this.todos.filter(t => t !== todo)
+      },
+      removeDoneTodos () {
+        this.todos = this.todos.filter(todo => !todo.complete)
       },
       changeFilter(filter) {
         this.filter = filter;
