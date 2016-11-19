0fbd1a14bb06b682f0711b4c2c886aad04089970
Omar Jbara <omar.Jbara2@gmail.com>
2016-11-20 00:09:56 +0100

    Changed editor config

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,64 @@
 <template>
-  <div id="app">
+	<div id="app">
 
-    <div class="ui fixed inverted menu">
-      <div class="ui container">
-        <a href="#" class="header item">
-          VueJS 2 Test
-        </a>
-        <a href="#" class="item">Home</a>
-        <div class="ui simple dropdown item">
-          Dropdown <i class="dropdown icon"></i>
-          <div class="menu">
-            <a class="item" href="#">Link Item</a>
-            <a class="item" href="#">Link Item</a>
-            <div class="divider"></div>
-            <div class="header">Header Item</div>
-            <div class="item">
-              <i class="dropdown icon"></i>
-              Sub Menu
-              <div class="menu">
-                <a class="item" href="#">Link Item</a>
-                <a class="item" href="#">Link Item</a>
-              </div>
-            </div>
-            <a class="item" href="#">Link Item</a>
-          </div>
-        </div>
-      </div>
-    </div>
+		<div class="ui fixed inverted menu">
+			<div class="ui container">
+				<a href="#" class="header item">
+					VueJS 2 Test
+				</a>
+				<a href="#" class="item">Home</a>
+				<div class="ui simple dropdown item">
+					Dropdown <i class="dropdown icon"></i>
+					<div class="menu">
+						<a class="item" href="#">Link Item</a>
+						<a class="item" href="#">Link Item</a>
+						<div class="divider"></div>
+						<div class="header">Header Item</div>
+						<div class="item">
+							<i class="dropdown icon"></i>
+							Sub Menu
+							<div class="menu">
+								<a class="item" href="#">Link Item</a>
+								<a class="item" href="#">Link Item</a>
+							</div>
+						</div>
+						<a class="item" href="#">Link Item</a>
+					</div>
+				</div>
+			</div>
+		</div>
 
-    <div class="ui container" style="margin-top: 50px;"><todos></todos></div>
+		<div class="ui container" style="margin-top: 50px;">
+			<todos></todos>
+		</div>
 
-  </div>
+	</div>
 
 </template>
 
 <script>
-import Todos from './components/Todos'
+	import Todos from './components/Todos'
 
-export default {
-  name: 'app',
-  components: {
-    Todos
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			Todos
+		}
+	}
 </script>
 
 <style>
-  body {
-    background: ghostwhite;
-  }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	body {
+		background: ghostwhite;
+	}
 
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+
+	}
 </style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,36 +1,42 @@
 <!--suppress HtmlUnknownAttribute, CheckTagEmptyBody, CheckTagEmptyBody -->
 <template>
-  <section class="todoapp">
-    <header class="header">
-      <input type="text" class="new-todo" v-model="newTodo" @keyup.enter="addTodo" placeholder="Ajoutez une tâche">
-    </header>
-    <section class="main">
-      <input type="checkbox" class="toggle-all" v-model="allDone">
-      <ul class="todo-list">
-        <li class="todo" v-for="todo in filteredTodos" :class="{'completed': todo.complete, 'editing': editing === todo}">
-          <div class="view">
-            <input type="checkbox" v-model="todo.complete" class="toggle">
-            <label @dblclick="editTodo(todo)">{{ todo.name }}</label>
-            <button @click.prevent="removeTodo(todo)" class="destroy"></button>
-          </div>
-          <input type="text" class="edit" v-model="todo.name" @blur="doneEditing()" @keyup.esc="cancelEditing()" @keyup.enter="doneEditing()" v-focus="editing === todo">
-        </li>
-      </ul>
-    </section>
-    <footer class="footer" v-if="todos.length > 0">
+	<section class="todoapp">
+		<header class="header">
+			<input type="text" class="new-todo" v-model="newTodo" @keyup.enter="addTodo"
+				   placeholder="Ajoutez une tâche">
+		</header>
+		<section class="main">
+			<input type="checkbox" class="toggle-all" v-model="allDone">
+			<ul class="todo-list">
+				<li class="todo" v-for="todo in filteredTodos"
+					:class="{'completed': todo.complete, 'editing': editing === todo}">
+					<div class="view">
+						<input type="checkbox" v-model="todo.complete" class="toggle">
+						<label @dblclick="editTodo(todo)">{{ todo.name }}</label>
+						<button @click.prevent="removeTodo(todo)" class="destroy"></button>
+					</div>
+					<input type="text" class="edit" v-model="todo.name" @blur="doneEditing()"
+						   @keyup.esc="cancelEditing()" @keyup.enter="doneEditing()" v-focus="editing === todo">
+				</li>
+			</ul>
+		</section>
+		<footer class="footer" v-if="todos.length > 0">
       <span class="todo-count">
         <strong> {{ remainingTodos }} </strong> tâches à faire
       </span>
-      <ul class="filters">
-        <li><a :class="{'selected': filter === 'all'}" @click.prevent="changeFilter('all')" href="#">Toutes</a></li>
-        <li><a :class="{'selected': filter === 'todo'}" @click.prevent="changeFilter('todo')" href="#">A faire</a></li>
-        <li><a :class="{'selected': filter === 'done'}" @click.prevent="changeFilter('done')" href="#">Faites</a></li>
-      </ul>
-      <button class="clear-completed" v-if="getDoneTodos.length > 0" @click.prevent="removeDoneTodos()">
-        Sup. tâches faites
-      </button>
-    </footer>
-  </section>
+			<ul class="filters">
+				<li><a :class="{'selected': filter === 'all'}" @click.prevent="changeFilter('all')" href="#">Toutes</a>
+				</li>
+				<li><a :class="{'selected': filter === 'todo'}" @click.prevent="changeFilter('todo')" href="#">A
+					faire</a></li>
+				<li><a :class="{'selected': filter === 'done'}" @click.prevent="changeFilter('done')"
+					   href="#">Faites</a></li>
+			</ul>
+			<button class="clear-completed" v-if="getDoneTodos.length > 0" @click.prevent="removeDoneTodos()">
+				Sup. tâches faites
+			</button>
+		</footer>
+	</section>
 
 </template>
 <style src="../assets/todos.css">
@@ -39,84 +45,84 @@
 </style>
 <script>
 
-  import Vue from 'vue';
+	import Vue from 'vue';
 
-  export default{
-    data () {
-      return {
-        newTodo: '',
-        todos: [],
-        filter: 'all',
-        oldTodoText: '',
-        editing: null
-      }
-    },
-    computed: {
-      remainingTodos () {
-        return this.todos.filter(todo => !todo.complete).length
-      },
-      filteredTodos () {
-        switch (this.filter) {
-          case 'all':
-            return this.todos.filter((todo, index, array) => array);
-            break;
-          case 'todo':
-            return this.todos.filter(todo => !todo.complete);
-            break;
-          case 'done':
-            return this.todos.filter(todo => todo.complete);
-            break;
-        }
+	export default{
+		data () {
+			return {
+				newTodo: '',
+				todos: [],
+				filter: 'all',
+				oldTodoText: '',
+				editing: null
+			}
+		},
+		computed: {
+			remainingTodos () {
+				return this.todos.filter(todo => !todo.complete).length
+			},
+			filteredTodos () {
+				switch (this.filter) {
+					case 'all':
+						return this.todos.filter((todo, index, array) => array);
+						break;
+					case 'todo':
+						return this.todos.filter(todo => !todo.complete);
+						break;
+					case 'done':
+						return this.todos.filter(todo => todo.complete);
+						break;
+				}
 
-      },
-      getDoneTodos() {
-        return this.todos.filter(todo => todo.complete);
-      },
-      allDone: {
-        get () {
-          return this.remainingTodos === 0
-        },
-        set (value) {
-          this.todos.forEach(todo => {
-            todo.complete = value
-          })
-        }
-      }
-    },
-    methods: {
-      addTodo() {
-        this.todos.push({name: this.newTodo, complete: false});
-        this.newTodo = '';
-      },
-      removeTodo(todo) {
-        this.todos = this.todos.filter(t => t !== todo)
-      },
-      editTodo (todo) {
-        this.oldTodoText = todo.name
-        this.editing = todo;
-      },
-      doneEditing() {
-        this.editing = null
-      },
-      cancelEditing() {
-        this.editing.name = this.oldTodoText
-        this.doneEditing()
-      },
-      removeDoneTodos () {
-        this.todos = this.todos.filter(todo => !todo.complete)
-      },
-      changeFilter(filter) {
-        this.filter = filter;
-      }
-    },
-    directives: {
-      focus (el, value) {
-        if(value){
-          Vue.nextTick(_ => {
-            el.focus()
-          })
-        }
-      }
-    }
-  }
+			},
+			getDoneTodos() {
+				return this.todos.filter(todo => todo.complete);
+			},
+			allDone: {
+				get () {
+					return this.remainingTodos === 0
+				},
+				set (value) {
+					this.todos.forEach(todo => {
+						todo.complete = value
+					})
+				}
+			}
+		},
+		methods: {
+			addTodo() {
+				this.todos.push({name: this.newTodo, complete: false});
+				this.newTodo = '';
+			},
+			removeTodo(todo) {
+				this.todos = this.todos.filter(t => t !== todo)
+			},
+			editTodo (todo) {
+				this.oldTodoText = todo.name
+				this.editing = todo;
+			},
+			doneEditing() {
+				this.editing = null
+			},
+			cancelEditing() {
+				this.editing.name = this.oldTodoText
+				this.doneEditing()
+			},
+			removeDoneTodos () {
+				this.todos = this.todos.filter(todo => !todo.complete)
+			},
+			changeFilter(filter) {
+				this.filter = filter;
+			}
+		},
+		directives: {
+			focus (el, value) {
+				if (value) {
+					Vue.nextTick(() => {
+						el.focus()
+					})
+				}
+			}
+		}
+	}
 </script>
