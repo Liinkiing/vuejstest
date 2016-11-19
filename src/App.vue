c1351b9b72ceeb4104bb5d9f6fc03ec28a69ea03
Omar Jbara <omar.jbara2@gmail.com>
2016-11-19 15:50:57 +0100

    Added semantic and Todos component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+
+    <div class="ui fixed inverted menu">
+      <div class="ui container">
+        <a href="#" class="header item">
+          <img class="logo" src="assets/images/logo.png">
+          Project Name
+        </a>
+        <a href="#" class="item">Home</a>
+        <div class="ui simple dropdown item">
+          Dropdown <i class="dropdown icon"></i>
+          <div class="menu">
+            <a class="item" href="#">Link Item</a>
+            <a class="item" href="#">Link Item</a>
+            <div class="divider"></div>
+            <div class="header">Header Item</div>
+            <div class="item">
+              <i class="dropdown icon"></i>
+              Sub Menu
+              <div class="menu">
+                <a class="item" href="#">Link Item</a>
+                <a class="item" href="#">Link Item</a>
+              </div>
+            </div>
+            <a class="item" href="#">Link Item</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Todos from './components/Todos'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Todos
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>this is template body {{ msg }}</div>
+    </div>
+</template>
+<style>
+
+
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+
+    }
+</script>
