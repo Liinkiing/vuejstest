13c55b711637229e217b7d5e552c0013f545bca9
Omar Jbara <omar.Jbara2@gmail.com>
2016-11-21 13:42:15 +0100

    Commit

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -10,39 +10,38 @@
 			<ul class="todo-list">
 				<li class="todo" v-for="todo in filteredTodos"
 					:class="{'completed': todo.complete, 'editing': editing === todo}">
-					<div class="view">
-						<input type="checkbox" v-model="todo.complete" class="toggle">
-						<label @dblclick="editTodo(todo)">{{ todo.name }}</label>
-						<button @click.prevent="removeTodo(todo)" class="destroy"></button>
-					</div>
-					<input type="text" class="edit" v-model="todo.name" @blur="doneEditing()"
-						   @keyup.esc="cancelEditing()" @keyup.enter="doneEditing()" v-focus="editing === todo">
+						<div class="view">
+							<input type="checkbox" v-model="todo.complete" class="toggle">
+							<label @dblclick="editTodo(todo)">{{ todo.name }}</label>
+							<button @click.prevent="removeTodo(todo)" class="destroy"></button>
+						</div>
+						<input type="text" class="edit" v-model="todo.name" @blur="doneEditing()"
+							   @keyup.esc="cancelEditing()" @keyup.enter="doneEditing()" v-focus="editing === todo">
 				</li>
 			</ul>
 		</section>
-		<footer class="footer" v-if="todos.length > 0">
+		<transition name="slide">
+			<footer class="footer" v-if="todos.length > 0">
       <span class="todo-count">
         <strong> {{ remainingTodos }} </strong> tâches à faire
       </span>
-			<ul class="filters">
-				<li><a :class="{'selected': filter === 'all'}" @click.prevent="changeFilter('all')" href="#">Toutes</a>
-				</li>
-				<li><a :class="{'selected': filter === 'todo'}" @click.prevent="changeFilter('todo')" href="#">A
-					faire</a></li>
-				<li><a :class="{'selected': filter === 'done'}" @click.prevent="changeFilter('done')"
-					   href="#">Faites</a></li>
-			</ul>
-			<button class="clear-completed" v-if="getDoneTodos.length > 0" @click.prevent="removeDoneTodos()">
-				Sup. tâches faites
-			</button>
-		</footer>
+				<ul class="filters">
+					<li><a :class="{'selected': filter === 'all'}" @click.prevent="changeFilter('all')" href="#">Toutes</a>
+					</li>
+					<li><a :class="{'selected': filter === 'todo'}" @click.prevent="changeFilter('todo')" href="#">A
+						faire</a></li>
+					<li><a :class="{'selected': filter === 'done'}" @click.prevent="changeFilter('done')"
+						   href="#">Faites</a></li>
+				</ul>
+				<button class="clear-completed" v-if="getDoneTodos.length > 0" @click.prevent="removeDoneTodos()">
+					Sup. tâches faites
+				</button>
+			</footer>
+		</transition>
 	</section>
 
 </template>
-<style src="../assets/todos.css">
-
-
-</style>
+<style src="../assets/todos.css"></style>
 <script>
 
 	import Vue from 'vue';
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import VueLocalStorage from 'vue-localstorage'
+import VueAnimatedList from 'vue-animated-list'
 
 Vue.use(VueLocalStorage);
+Vue.use(VueAnimatedList);
+
 
 /* eslint-disable no-new */
 new Vue({
