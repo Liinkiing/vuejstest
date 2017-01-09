5a9be1a566d61c06853e454ee7b97bb7c9d04bee
Omar Jbara <omar.Jbara2@gmail.com>
2017-01-09 23:49:54 +0100

    WIP Examples pages

--- src/components/pages/Examples.vue
+++ src/components/pages/Examples.vue
@@ -1,71 +1,144 @@
 <template>
-		<div class="container">
-			<h2>Exemples de composants créés avec Vuejs</h2>
-			<p>Durant mon temps libre, j'ai appris petit à petit, à utiliser VueJS. J'ai tout de suite accroché à la façon dont VueJS nous permettait de créer des composants très simplements et surtout ces derniers sont
-				<strong>modulable</strong>, c'est-à-dire que je peux désormais très facilement inclure un composant VueJS dans une autre application. Vous trouverez donc sur cette page les différents composants que j'ai créée de A à Z (apparence comprise)
-				avec le code source nécessaire au rendu de ce composant.
-			</p>
-			<h3>Carousel</h3>
-			<carousel :autoplay="true" :slide-duration="2000">
-				<carousel-slide v-for="n in 8" :index="n - 1">
-					<h1>Je suis la slide n {{n}}</h1>
-				</carousel-slide>
-			</carousel>
-			<h3>Tabs components</h3>
-			<tabs-container>
-				<tab header="Je suis un onglet" :index="0">Je suis le contenu</tab>
-				<tab header="Et moi un autre" :index="1">Et moi l'autre</tab>
-			</tabs-container>
-			<pre class="language-markup"><code>
+	<div class="container">
+		<h2>Exemples de composants créés avec Vuejs</h2>
+		<p>Durant mon temps libre, j'ai appris petit à petit, à utiliser VueJS. J'ai tout de suite accroché à la façon
+			dont VueJS nous permettait de créer des composants très simplements et surtout ces derniers sont
+			<strong>modulable</strong>, c'est-à-dire que je peux désormais très facilement inclure un composant VueJS
+			dans une autre application. Vous trouverez donc sur cette page les différents composants que j'ai créée de A
+			à Z (apparence comprise)
+			avec le code source nécessaire au rendu de ce composant.
+		</p>
+		<h3>Carousel</h3>
+		<carousel :autoplay="true" :slide-duration="2000">
+			<carousel-slide v-for="n in 8" :index="n - 1">
+				<h1>Je suis la slide n {{n}}</h1>
+			</carousel-slide>
+		</carousel>
+		<pre><code class="html">
+&lt;carousel :autoplay="true" :slide-duration="2000">
+	&lt;carousel-slide v-for="n in 8" :index="n - 1">
+		&lt;h1>Je suis la slide n {{n}}&lt;/h1>
+	&lt;/carousel-slide>
+&lt;/carousel>
+		</code></pre>
+		<h3>Tabs components</h3>
+		<tabs-container>
+			<tab header="Je suis un onglet" :index="0">Je suis le contenu</tab>
+			<tab header="Et moi un autre" :index="1">Et moi l'autre</tab>
+		</tabs-container>
+		<pre><code class="html">
 &lt;tabs-container>
 	&lt;tab header="Je suis un onglet" :index="0">Je suis le contenu&lt;/tab>
 	&lt;tab header="Et moi un autre" :index="1">Et moi l'autre&lt;/tab>
 &lt;/tabs-container>
 			</code></pre>
-			<h3>Tags components</h3>
-			<tags :max="5"></tags>
-			<h3>Lightbox</h3>
+		<h3>Tags components</h3>
+		<tags :max="5"></tags>
+		<pre><code class="html">
+&lt;tags :max="5">&lt;/tags>
+			</code></pre>
+		<h3>Lightbox</h3>
 
-			<a href="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" v-lightbox="{group: 'nature', title: 'Shaimes Mirenda', 'desc': 'Shaimes le bg'}">
-				<img src="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" width="266" height="150" alt="">
-			</a>
+		<a href="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg"
+		   v-lightbox="{group: 'nature', title: 'Titre image', 'desc': 'Description de l\'image'}">
+			<img src="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" width="266" height="150" alt="">
+		</a>
 
-			<a href="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" v-lightbox="{group: 'nature', title: 'Je suis un titre et la première image'}">
-				<img src="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" width="266" height="150" alt="">
-			</a>
-			<a href="http://wallpapercave.com/wp/eaFUfBE.jpg" v-lightbox="{group: 'nature', desc: 'Je suis une description'}">
-				<img src="http://wallpapercave.com/wp/eaFUfBE.jpg" width="266" height="150" alt="">
-			</a>
-			<a href="http://hdwallpaperbackgrounds.net/wp-content/uploads/2016/11/Full-Screen-HD-Wallpapers-7.jpg" v-lightbox="{group: 'nature', title: 'Titre', desc: 'Je suis une description'}">
-				<img src="http://hdwallpaperbackgrounds.net/wp-content/uploads/2016/11/Full-Screen-HD-Wallpapers-7.jpg" width="266" height="150" alt="">
-			</a>
-			<h4>Groupe 'ville'</h4>
-			<a href="http://www.pixelstalk.net/wp-content/uploads/2016/05/Full-HD-Desktop-Wallpaper.jpg" v-lightbox="{group: 'ville' }">
-				<img src="http://www.pixelstalk.net/wp-content/uploads/2016/05/Full-HD-Desktop-Wallpaper.jpg" width="266" height="150" alt="">
-			</a>
-			<a href="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-21.png" v-lightbox="{group: 'ville'}">
-				<img src="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-21.png" width="266" height="150" alt="">
-			</a>
-			<a href="http://wallpapercave.com/wp/NIAnj0Q.jpg" v-lightbox="{group: 'ville', title: 'City Skyline', desc: 'Je suis une description'}">
-				<img src="http://wallpapercave.com/wp/NIAnj0Q.jpg" width="266" height="150" alt="">
-			</a>
-			<a href="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-28.png" v-lightbox="{group: 'ville'}">
-				<img src="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-28.png" width="266" height="150" alt="">
-			</a>
-			<a href="http://www.howtogeek.com/wp-content/uploads/gg/up/sshot4ddf8753257e6.jpg" v-lightbox="{group: 'ville', title: 'City Skyline'}">
-				<img src="http://www.howtogeek.com/wp-content/uploads/gg/up/sshot4ddf8753257e6.jpg" width="266" height="150" alt="">
-			</a>
-			<a href="http://www.siwallpaperhd.com/wp-content/uploads/2015/05/New-York-City-Skyline-Wallpaper-HD-16-Image-1024x576.jpg" v-lightbox="{group: 'ville'}">
-				<img src="http://www.siwallpaperhd.com/wp-content/uploads/2015/05/New-York-City-Skyline-Wallpaper-HD-16-Image-1024x576.jpg" width="266" height="150" alt="">
-			</a>
-			<a href="http://www.intrawallpaper.com/static/images/eiffel-tower-wallpaper-18_TJ30uc8.jpg" v-lightbox="{title: 'Paris', desc:'Jsuis une desc', group: 'ville'}">
-				<img src="http://www.intrawallpaper.com/static/images/eiffel-tower-wallpaper-18_TJ30uc8.jpg" width="266" height="150" alt="">
-			</a>
+		<a href="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg"
+		   v-lightbox="{group: 'nature', title: 'Je suis un titre et la première image'}">
+			<img src="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" width="266" height="150" alt="">
+		</a>
+		<a href="http://wallpapercave.com/wp/eaFUfBE.jpg"
+		   v-lightbox="{group: 'nature', desc: 'Je suis une description'}">
+			<img src="http://wallpapercave.com/wp/eaFUfBE.jpg" width="266" height="150" alt="">
+		</a>
+		<a href="http://hdwallpaperbackgrounds.net/wp-content/uploads/2016/11/Full-Screen-HD-Wallpapers-7.jpg"
+		   v-lightbox="{group: 'nature', title: 'Titre', desc: 'Je suis une description'}">
+			<img src="http://hdwallpaperbackgrounds.net/wp-content/uploads/2016/11/Full-Screen-HD-Wallpapers-7.jpg"
+				 width="266" height="150" alt="">
+		</a>
+		<h4>Groupe 'ville'</h4>
+		<a href="http://www.pixelstalk.net/wp-content/uploads/2016/05/Full-HD-Desktop-Wallpaper.jpg"
+		   v-lightbox="{group: 'ville' }">
+			<img src="http://www.pixelstalk.net/wp-content/uploads/2016/05/Full-HD-Desktop-Wallpaper.jpg" width="266"
+				 height="150" alt="">
+		</a>
+		<a href="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-21.png"
+		   v-lightbox="{group: 'ville'}">
+			<img src="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-21.png" width="266"
+				 height="150" alt="">
+		</a>
+		<a href="http://wallpapercave.com/wp/NIAnj0Q.jpg"
+		   v-lightbox="{group: 'ville', title: 'City Skyline', desc: 'Je suis une description'}">
+			<img src="http://wallpapercave.com/wp/NIAnj0Q.jpg" width="266" height="150" alt="">
+		</a>
+		<a href="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-28.png"
+		   v-lightbox="{group: 'ville'}">
+			<img src="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-28.png" width="266"
+				 height="150" alt="">
+		</a>
+		<a href="http://www.howtogeek.com/wp-content/uploads/gg/up/sshot4ddf8753257e6.jpg"
+		   v-lightbox="{group: 'ville', title: 'City Skyline'}">
+			<img src="http://www.howtogeek.com/wp-content/uploads/gg/up/sshot4ddf8753257e6.jpg" width="266" height="150"
+				 alt="">
+		</a>
+		<a href="http://www.siwallpaperhd.com/wp-content/uploads/2015/05/New-York-City-Skyline-Wallpaper-HD-16-Image-1024x576.jpg"
+		   v-lightbox="{group: 'ville'}">
+			<img
+				src="http://www.siwallpaperhd.com/wp-content/uploads/2015/05/New-York-City-Skyline-Wallpaper-HD-16-Image-1024x576.jpg"
+				width="266" height="150" alt="">
+		</a>
+		<a href="http://www.intrawallpaper.com/static/images/eiffel-tower-wallpaper-18_TJ30uc8.jpg"
+		   v-lightbox="{title: 'Paris', desc:'Je suis une autre description', group: 'ville'}">
+			<img src="http://www.intrawallpaper.com/static/images/eiffel-tower-wallpaper-18_TJ30uc8.jpg" width="266"
+				 height="150" alt="">
+		</a>
 
-			<h3>Todo</h3>
-			<todos></todos>
+		<pre><code class="html">
+&lt;a href="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" v-lightbox="{group: 'nature', title: 'Titre image', 'desc': 'Description de l\'image'}">
+	&lt;img src="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" width="266" height="150" alt="">
+&lt;/a>
 
-		</div>
+&lt;a href="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" v-lightbox="{group: 'nature', title: 'Je suis un titre et la première image'}">
+	&lt;img src="http://cdn.wallpapersafari.com/33/5/9TPZ3I.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://wallpapercave.com/wp/eaFUfBE.jpg" v-lightbox="{group: 'nature', desc: 'Je suis une description'}">
+	&lt;img src="http://wallpapercave.com/wp/eaFUfBE.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://hdwallpaperbackgrounds.net/wp-content/uploads/2016/11/Full-Screen-HD-Wallpapers-7.jpg" v-lightbox="{group: 'nature', title: 'Titre', desc: 'Je suis une description'}">
+	&lt;img src="http://hdwallpaperbackgrounds.net/wp-content/uploads/2016/11/Full-Screen-HD-Wallpapers-7.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;h4>Groupe 'ville'&lt;/h4>
+&lt;a href="http://www.pixelstalk.net/wp-content/uploads/2016/05/Full-HD-Desktop-Wallpaper.jpg" v-lightbox="{group: 'ville' }">
+	&lt;img src="http://www.pixelstalk.net/wp-content/uploads/2016/05/Full-HD-Desktop-Wallpaper.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-21.png" v-lightbox="{group: 'ville'}">
+	&lt;img src="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-21.png" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://wallpapercave.com/wp/NIAnj0Q.jpg" v-lightbox="{group: 'ville', title: 'City Skyline', desc: 'Je suis une description'}">
+	&lt;img src="http://wallpapercave.com/wp/NIAnj0Q.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-28.png" v-lightbox="{group: 'ville'}">
+	&lt;img src="http://allfreedesigns.com/wp-content/uploads/2011/08/city-skyline-wallpapers-28.png" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://www.howtogeek.com/wp-content/uploads/gg/up/sshot4ddf8753257e6.jpg" v-lightbox="{group: 'ville', title: 'City Skyline'}">
+	&lt;img src="http://www.howtogeek.com/wp-content/uploads/gg/up/sshot4ddf8753257e6.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://www.siwallpaperhd.com/wp-content/uploads/2015/05/New-York-City-Skyline-Wallpaper-HD-16-Image-1024x576.jpg" v-lightbox="{group: 'ville'}">
+	&lt;img src="http://www.siwallpaperhd.com/wp-content/uploads/2015/05/New-York-City-Skyline-Wallpaper-HD-16-Image-1024x576.jpg" width="266" height="150" alt="">
+&lt;/a>
+&lt;a href="http://www.intrawallpaper.com/static/images/eiffel-tower-wallpaper-18_TJ30uc8.jpg" v-lightbox="{title: 'Paris', desc:'Je suis une autre description', group: 'ville'}">
+	&lt;img src="http://www.intrawallpaper.com/static/images/eiffel-tower-wallpaper-18_TJ30uc8.jpg" width="266" height="150" alt="">
+&lt;/a>
+			</code></pre>
+
+		<h3>Todo</h3>
+		<todos></todos>
+		<pre><code class="html">
+&lt;todos>&lt;/todos>
+		</code></pre>
+
+	</div>
 </template>
 <style lang="scss">
 
@@ -80,10 +153,10 @@
 			position: absolute;
 			left: 0;
 			bottom: -10px;
-			 height: 3px;
-			 width: 100%;
+			height: 3px;
+			width: 100%;
 			background: whitesmoke;
-		 }
+		}
 	}
 
 	.message.info {
@@ -127,10 +200,11 @@
 		},
 
 		mounted() {
+			hljs.initHighlighting();
 			this.language = this.languages[0];
 			window.setInterval(() => {
 				let i = this.languages.indexOf(this.language) + 1;
-				if(i > this.languages.length - 1) i = 0;
+				if (i > this.languages.length - 1) i = 0;
 				this.language = this.languages[i];
 			}, 3000);
 		}
